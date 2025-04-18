3f7df6082e7f86ada13ce46a64ebfa1b43cc594e
SONALI TIWARY <sonali.dtg1@gmail.com>
2025-04-18 21:53:25 +0530

    Update SQL1.sql

--- SQL1.sql
+++ SQL1.sql
@@ -1,3 +1,4 @@
+--Create Table "Studets"
 CREATE TABLE Studets(
       Student_id SERIAL PRIMARY KEY,
 	  Student_name VARCHAR(100) NOT NULL,
@@ -6,16 +7,17 @@ CREATE TABLE Studets(
 	  Percentage NUMERIC(2,2)
 );
 
+--Retrive data
 SELECT * FROM Students;
 
-
+--Insert data into table
 INSERT INTO Students(Student_name,Admission_date,Student_class,Percentage)
             VALUES('Ajit sharma','2016-08-13',5,81.2),
 			('Priya desai','2016-07-30',5,83.61),
 			('Rajesh kumar','2016-08-18',5,72.13),
 			('Sneha patel','2016-07-27',5,69.31),
 			('Vikram singh','2016-08-07',5,74.00);
-			
+-- Rename table "Stydets" to "Students"			
 ALTER TABLE Studets
 RENAME TO Students;
 
